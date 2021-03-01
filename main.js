4f581467a2276767ac9fd73717086e903da28751
leejh96 <dlwngur96@naver.com>
2021-03-01 22:15:54 +0900

    웹 영어단어장 navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const navbarMenu = document.querySelector('.navbar_menu');
+const loginLinks = document.querySelector('.navbar_link');
+
+toggleBtn.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('active');
+    loginLinks.classList.toggle('active');
+});
