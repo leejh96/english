8b1b7a6b49c22b56ed495503dd4561b68b85243a
leejh96 <dlwngur96@naver.com>
2021-03-04 00:27:40 +0900

    로그인 화면 서버연결

--- app.js
+++ app.js
@@ -3,29 +3,25 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
 require('dotenv').config();
 
-const indexRouter = require('./routes/index')
-// const {sequelize} = require('./models');
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
 app.set('port', process.env.PORT||8000);
-// app.use(logger('dev'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express(urlencoded({ extended: false})));
 app.use('/', indexRouter);
-// app.use('/login', loginRouter);
-
+app.use('/login', loginRouter);
 app.use((req, res, next)=>{
-    res.status(404).send('NOT FOUND');
+    res.status(404).send('요청하신 페이지를 찾을 수 없습니다.');
 })
 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
-
+    res.status(500).send('서버 에러!');
+});
 app.listen(app.get('port'), ()=>{
     console.log(`${app.get('port')}포트 서버가 실행중입니다.`)
 })
--- routes/login.js
+++ routes/login.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', (req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../views', 'index.html'))
+    res.sendFile(path.join(__dirname, '../views', 'login.html'));
 })
-
 
 module.exports = router;
