ce5e869b679e658d7159bcd175625c80ce5fc34a
leejh96 <dlwngur96@naver.com>
2021-03-09 18:30:06 +0900

    회원가입 구현

--- routes/signup.js
+++ routes/signup.js
@@ -9,23 +9,33 @@ router.get('/', (req, res, next)=>{
     res.render('signup');
 });
 
-// router.post('/', (req, res,next)=>{
-//     console.log(User);
-//     const id = req.body.id;
-//     const password = req.body.password;
-//     User.findOne({ where : { loginId : id}})
-//     .then(user => {
-//         if (user.loginId === id){
-//             if(user.password === password){
-//                 return res.json({
-//                     success : true
-//                 })
-//             }
-//         }
-//         return res.json({
-//             success : false
-//         })
-//     })
-// })
+router.post('/', async (req, res, next)=>{
+    const name = req.body.name;
+    const id = req.body.id;
+    const password = req.body.password;
+    const existId = await User.findOne({ where : { loginId : id }})
+    if (existId){
+        return res.json({
+            success : false,
+            message : "이미 존재하는 아이디 입니다."
+        })
+    }
+    const existPassword = await User.findOne({where : {password}});
+    console.log(existPassword);
+    if (existPassword){
+        return res.json({
+            success : false,
+            message : "이미 존재하는 비밀번호 입니다."
+        })
+    }
+    User.create({
+        name,
+        loginId : id,
+        password,
+    });
+    return res.json({
+        success : true
+    })
+})
 
 module.exports = router;
