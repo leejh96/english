0dfbd8d4afffa11e26d3140b4e0f45e21dbe1373
leejh96 <dlwngur96@naver.com>
2021-04-09 15:45:52 +0900

    퀴즈 타이머 만들기

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -2,61 +2,76 @@ const webSocket = new WebSocket('ws://localhost:3000/quiz');
 const spelling = document.querySelector('#spelling');
 const btn = document.querySelectorAll('.btn');
 const correctCount = document.querySelector('#correctCount');
-webSocket.onmessage = (e)=>{
-    const words = JSON.parse(e.data);
-    let randomIndex = []
-    let cnt = 1
-    let quizCnt = 10;
-    let count = 0;
-    while(cnt !== 10){
-        while(randomIndex.length !== 4)
-        {
-            let randomNumber = Math.floor(Math.random()*words.length);
-            if(!randomIndex.includes(randomNumber)){
-                randomIndex.push(randomNumber);
-            }
-        }
-        spelling.innerHTML = words[randomIndex[0]].spelling;
-        let useValue = [];
-        let i = 0
-        while (useValue.length !== 4){
-            let randomValue = Math.floor(Math.random()*randomIndex.length);//0123
-            if(!useValue){
-                btn[i].value = words[randomValue].meaning;
-                i += 1
-                useValue.push(randomValue);
+const restart = document.querySelector('#restart');
+const timer = document.querySelector('#timer');
 
-            }else{
-                if(!useValue.includes(randomValue)){
-                    btn[i].value = words[randomValue].meaning;
-                    i += 1
-                    useValue.push(randomValue);
-                }
+let randomIndex = []
+let quizCnt = 10;
 
-            }
+function spellingInput(words, randomIndex, cnt){
+    spelling.innerHTML = words[randomIndex[cnt]].spelling;    
+}
+function btnInput(words, randomIndex, btn, quizCnt){
+    let i = 0;
+    let useValue = [];
+    while (useValue.length !== quizCnt){
+        let randomValue = Math.floor(Math.random()*randomIndex.length);
+        if(!useValue.includes(randomValue)){
+            btn[i].value = words[randomValue].meaning;
+            i += 1
+            useValue.push(randomValue);
         }
-
-        correctCount.innerHTML = `맞춘 갯수 : ${count} / ${quizCnt}`
-        for(let i = 0; i<4; i++){
-            btn[i].addEventListener('click',()=>{ 
-                if(spelling.innerHTML === words[randomIndex[0]].spelling &&
-                    btn[i].value === words[randomIndex[0]].meaning ){
-                    count += 1
-                    correctCount.innerHTML = `맞춘 갯수 : ${count} / ${quizCnt}`
-                }
-            });
-        }
-        cnt += 1
     }
-    if(count > 7){
-        alert('잘하시네요');
-        location.href= '/quiz'
-    }else if(count > 4){
-        alert('보통입니다');
-        location.href= '/quiz'
-
-    }else{
-        alert('분발하세요');
-        location.href= '/quiz'
+}
+function btnEvnet(quizCnt, settimeout){
+    let count = 0
+    for(let j = 0; j<4; j++){
+        btn[j].addEventListener('click',()=>{ 
+            count += 1
+            correctCount.innerHTML = `맞춘 갯수 : ${count} / ${quizCnt}`
+            clearTimeout(settimeout);
+        });
+    }
+}
+function timerValue(time){
+    for(let i = 0; i <5; i++){
+        setTimeout(() => {
+            time = parseInt(time);
+            timer.innerHTML = time;
+            time -= 1;
+        }, i* 1000);
     }
 };
+
+function randomEnglish(randomIndex, quizCnt, words){
+    while(randomIndex.length !== quizCnt)
+    {
+        let randomNumber = Math.floor(Math.random()*words.length); //0123
+        if(!randomIndex.includes(randomNumber)){  //randomindex에 없나?
+            randomIndex.push(randomNumber);// 그렇다면 randomindex에 넣자
+        }
+    }
+    return randomIndex;
+}
+webSocket.onmessage = (e)=>{
+    const words = JSON.parse(e.data);
+    let time = 5;
+    timer.innerHTML = time;
+    if (words.length < quizCnt){
+        quizCnt = words.length;
+    }
+    randomIndex = randomEnglish(randomIndex, quizCnt, words);
+    for(let cnt = 0; cnt < quizCnt; cnt++){
+        const settimeout = setTimeout(()=> {
+            spellingInput(words, randomIndex, cnt);
+            btnInput(words, randomIndex, btn, quizCnt);
+            btnEvnet(quizCnt, settimeout);
+            timerValue(time);
+        }, cnt* 5000);
+    }
+};
+restart.addEventListener('click', ()=>{
+    location.href = '/quiz';
+});
+
+
--- routes/quiz.js
+++ routes/quiz.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const {Word} = require('../models');
 
 router.get('/', async(req, res, next)=>{
-    if (!req.user){
-        return res.redirect('/login');
-    }
+    // if (!req.user){
+    //     return res.redirect('/login');
+    // }
     const words = await Word.findAll();
     res.render('quiz', {words});
 });
